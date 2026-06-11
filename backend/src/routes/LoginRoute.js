7d69f55d3f2312d8e6e6d0ee2ffed24279a5e463
ZeenatIslam <zeenatislam011@gmail.com>
2026-06-12 03:10:56 +0530

    testing

--- backend/src/routes/LoginRoute.js
+++ backend/src/routes/LoginRoute.js
@@ -1,11 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const {
-  register,
-  login,
-  googleLogin,
-} = require("../controllers/authController")
+const {register,login,googleLogin} = require("../controllers/AuthController")
 
 router.post("/register", register)
 router.post("/login", login)
