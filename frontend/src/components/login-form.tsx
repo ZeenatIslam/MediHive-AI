89b22f9a2c01544f471eba021f13c9f5f643b33e
ZeenatIslam <zeenatislam011@gmail.com>
2026-06-11 22:33:02 +0530

    testing

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -43,7 +43,7 @@ export function LoginForm({
       setLoading(true)
 
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://medihive-ai-backend.onrender.com/api/auth/login",
         {
           email,
           password,
@@ -72,7 +72,7 @@ export function LoginForm({
   ) => {
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/google",
+        "https://medihive-ai-backend.onrender.com/api/auth/google",
         {
           credential: credentialResponse.credential,
         }
